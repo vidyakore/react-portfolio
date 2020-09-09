b342ba3ce2b8536aa9da866f225e99b43152e7c6
vidyakore <56337652+vidyakore@users.noreply.github.com>
2020-09-09 12:54:02 +0530

    build app

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
+            <li><span>Modified with love by the Clever Programmer team ♥ </span></li>
            </ul>
 
            <ul className="copyright">
